3fc1ba486f179cdc64b49e6c55abde5ac75ae208
Max <pptp99@mail.ru>
2019-10-07 15:00:03 +0300

    Initial commit

---
+++ src/Gradeability.java
@@ -0,0 +1,3 @@
+public interface Gradeability {
+     void actionChoose(Actionable g);
+}
---
+++ src/Track.java
@@ -0,0 +1,13 @@
+public class Track implements Gradeability{
+    private int length;
+    public Track(int length){
+        this.length=length;
+    }
+    public int getLength(){
+        return length;
+    }
+    @Override
+    public void actionChoose(Actionable g) {
+    g.action(this);
+    }
+}
