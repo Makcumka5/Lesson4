35aab47e11cd34d43509fbad22f7fd4585d27884
Max <pptp99@mail.ru>
2019-10-17 13:20:28 +0300

    Первое дз и то с помощью, будем работать!

---
+++ src/Lesson04/Chat.java
@@ -0,0 +1,61 @@
+package Lesson04;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Chat extends JFrame {
+
+    JTextArea textArea;
+    JTextField textMassage;
+
+    public Chat() {
+        setTitle("Main");
+        setBounds(800, 300, 400, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel centerPanel = new JPanel();
+        add(centerPanel, BorderLayout.CENTER);
+        centerPanel.setBackground(Color.gray);
+        centerPanel.setLayout(new BorderLayout());
+
+        JPanel bottonPanel = new JPanel();
+        add(bottonPanel, BorderLayout.SOUTH);
+        bottonPanel.setBackground(Color.darkGray);
+        bottonPanel.setPreferredSize(new Dimension(1, 40));
+        bottonPanel.setLayout(new BorderLayout());
+
+        JButton startButton = new JButton("Отправить");
+        bottonPanel.add(startButton, BorderLayout.EAST);
+
+        textArea = new JTextArea();
+        JScrollPane textAreaScroll = new JScrollPane(textArea);
+        centerPanel.add(textAreaScroll, BorderLayout.CENTER);
+        textArea.setEditable(false);
+
+        textMassage = new JTextField();
+        bottonPanel.add(textMassage, BorderLayout.CENTER);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addText();
+            }
+        });
+
+        textMassage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addText();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    void addText() {
+        textArea.append(textMassage.getText() + "\n");
+        textMassage.setText("");
+    }
+}
